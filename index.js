023680acbd256637dee9f7fbceece307c25dece3
eric guzman <xericguzman@gmail.com>
2022-06-28 07:49:01 -0400

    feel free to axolotol questiones

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 console.log("ok this works not fucked up gj dawg");
 //api
 let api =
-  "https://api.giphy.com/v1/gifs/search?api_key=P7JZb1su5AOUPUjlc1zWpqSFegSuG5rE&q=Michael+Jackson&limit=50&offset=0&rating=r&lang=en";
+  "https://api.giphy.com/v1/gifs/search?api_key=P7JZb1su5AOUPUjlc1zWpqSFegSuG5rE&q=michael+jackson&limit=&offset=0&rating=r&lang=en";
 
 //contents
 let button = document.querySelector(".button");
